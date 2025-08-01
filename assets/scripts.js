a297131f5bfc835cd7e785535c289ac106c70ac7
Shahmir Khan <100615405+shahmeerkhan12@users.noreply.github.com>
2025-08-01 12:37:56 +0500

    Update scripts.js
    changing titles on the banner

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,14 @@
+const changingText = document.getElementById("title-name");
 
+const title-names = ["I'm Shamir Khan","I'm a Front-End Developer", "I'm a Web Developer" ];
+
+let current-text-index = 0
+
+function changeText(){
+  changingText.textContent = title-names[current-text-index];
+  current-text-index += 1;
+if(current-text-index >= title-name.length){
+ current-text-index = 0;
+}
+}
+setInterval = (changeText, 2000);
